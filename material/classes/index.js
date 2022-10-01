0e9446139eba1330b4300f768dd23a11ae3bff92
karumundo <karumundo@gmail.com>
2022-10-01 13:13:48 -0300

    Cógidos gerados em sala de aula

---
+++ material/classes/index.js
@@ -0,0 +1,32 @@
+// Criar uma "classificação" da nossa entidade para modelar os nossos atributos.
+class Users {};
+
+//Como atribuímos valores a nossa classe?
+class User{
+    constructor(name, email, password){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+    }
+ 
+    //Método get - retorna o valor do parâmetro do objeto chamado
+    getName(){
+        return `Seja bem vinde ${this.name}`
+    }
+
+    //Método length
+    getNameLength(){
+        return `${this.name}, o tamanho do teu primeiro nome é ${this.name.length}`
+    }
+}
+//Classificamos aquele objeto, criamos o modelo daquela entidade.
+//O this indica o contexto, referencia para que a classe consiga criar o objeto com aquele valor. Ex.:
+//Ao receber algum valor de nome, criar o objeto e inserir esse valor à propriedade nome. 
+
+//Chamamos de instanciar o objeto a criação do objeto em si
+const user1 = new User("Caroline", "caroline@gmail.com", "123456");
+const user2 = new User("Gustavo", "gustavo@gmail.com", "1234567");
+const user3 = new User("Prana", "praninha@gmail.com", "1234567");
+
+console.log(user3.getNameLength())
+
