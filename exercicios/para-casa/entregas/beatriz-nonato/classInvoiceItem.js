338fdbb57916fdfb55c3e3b673ebf02f2dfdef38
Beatriz Nonato <bia_nonato@icloud.com>
2022-10-05 16:12:34 -0300

    <fe0f> feat: create class and method InvoiceItem

---
+++ exercicios/para-casa/entregas/beatriz-nonato/classInvoiceItem.js
@@ -0,0 +1,42 @@
+/*Escreva uma classe chamada InvoiceItem,
+que modela um item de uma compra, com id, description, quantity e price.
+
+criar metodo preço unitario vezes quantidade
+*/
+
+class InvoiceItem {
+    constructor(id, description, quantity, price) {
+        this.id = id;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    getProductPriceResult() {
+        const finalProductPrice = this.price * this.quantity
+        return `R$${finalProductPrice}`
+    }
+
+    getProductDescription() {
+        return `Descrição: ${this.description}`
+    }
+
+    getProductID() {
+        return `ID: ${this.id}`
+    }
+
+    getProductUnitPrice() {
+        return `Preço Unitário: R$${this.price}`
+    }
+
+    getProductQuantity() {
+        return `Quantidade: ${this.quantity}`
+    }
+
+    getDetailInvoiceItem() {
+        const details = this.getProductID() + '\n' + this.getProductDescription() + '\n' + this.getProductUnitPrice() + '\n' + this.getProductQuantity()
+        return details
+    }
+}
+
+const comedouroRainbow = new InvoiceItem('0001', 'seu gato merece um potinho lindo para fazer as refeições. o Comedouro Petiscos chegou para cumprir essa função. além de útil, ele também decora o seu lar com charme.', 30, 50) 
