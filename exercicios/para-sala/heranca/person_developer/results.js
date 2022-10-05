a54eaa7a1d2000b6b3bc4d5fbfa6e908feb45a88
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-10-04 21:13:25 -0300

    Exercices of Herança

---
+++ exercicios/para-sala/heranca/person_developer/results.js
@@ -0,0 +1,6 @@
+import { Developer } from './Developer.js'
+
+const createPerfilDani = new Developer("Danielle", "dani@email.com", "(11)9999-2212", ["html", "css", "Javascript"])
+
+console.log(createPerfilDani)
+console.log(createPerfilDani.getName())
