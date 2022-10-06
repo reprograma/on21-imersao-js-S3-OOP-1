0037de7d1d3c4be03d8087e93f18def604056d6a
mabiamabia <duartecostap@gmail.com>
2022-10-06 13:54:08 -0300

    Cria projeto

---
+++ projeto-teste/atracoes.js
@@ -0,0 +1,14 @@
+//criar uma classe para cada componente
+//criar botoes para cada componente
+//descobrir sobre o que vc quer fazer o site
+
+class Atracao {
+    constructor(nomeAtracao, dia, tempoApresentacao){
+        this.nomeAtracao = nomeAtracao;
+        this.dia = dia;
+        this.tempoApresentacao = tempoApresentacao;
+    }
+    getDia(){
+        return console.log(dia);
+    }
+}
