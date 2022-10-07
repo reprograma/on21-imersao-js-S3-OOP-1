bb0a8736b214b7e527b42e9670e4c075c7f249fe
mabiamabia <duartecostap@gmail.com>
2022-10-06 21:16:07 -0300

    Finaliza atividade

--- exercicios/para-casa/entregas/mabia/resolucao.js
+++ exercicios/para-casa/entregas/mabia/resolucao.js
@@ -8,8 +8,22 @@ class InvoiceItem {
   getID() {
     return this.id;
   }
+  getDesc() {
+    return this.description;
+  }
+  getQuantity() {
+    return this.quantity;
+  }
+  getUnitPrice() {
+    return this.price;
+  }
 }
 
 const item = new InvoiceItem(1, "arroz integral", 3, 25);
 
+const item2 = new InvoiceItem(2, "Sal de banho", 2, 150);
+
 console.log(item.getID());
+console.log(item2.getID());
+console.log(item.getDesc());
+console.log(item2.getDesc());
