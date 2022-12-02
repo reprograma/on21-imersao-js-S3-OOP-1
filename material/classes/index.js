aabff6bcd442fc2ae6a96171cfc86682cf76ece0
Silvovisk <vascsilvia@gmail.com>
2022-12-02 00:10:16 -0300

    primeira parte do exercicio

--- material/classes/index.js
+++ material/classes/index.js
@@ -5,14 +5,17 @@
 } */
 
 //Como atribuimos valores a nossa classe?
+//atributos = caracteristicas 
 
 class User{
     constructor(name, email, password){ // (name, email, password) -----> Quando esta aqui são "atributos"
         this.name = name;
         this.email = email;          // this.email = email ----> quando esta aqui estou passando um "parâmetro", estou passando um parâmetro e vou atribuir a um atributo
-        this.password = password; 
+        this.password = password;    // o this faz uma referencia ao nosso atributo, estamos contextualizando o atributo
+                                     // o que vamos utilizar no nosso método são os parametros, os 'this'. 
     }
 
+    //Métodos são funções que nos auxiliam em uma determinada ação no nosso objeto
     getName(){
         return `Seja bem vinde, ${this.name}`
     }
@@ -34,6 +37,9 @@ const objLiteralUser2 = {
 const objLiteralUser3 = {
     name: "Mabiá"
 }
+
+//Objeto que foi instânciado, NEW é instânciar
+//Eu estou pegando aquela classe acima para criar um objeto (new = instância) 
 const user1 = new User("Jéssica", "oskojess@gmail.com", "senha12#");
 const user2 = new User("Jéssica", "oskojess@gmail.com", "senha12#");
 const user3 = new User("Jéssica", "oskojess@gmail.com", "senha12#");
