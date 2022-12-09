77683cc7bf850ed553252b6ebd43ba2ad67c922e
Silvovisk <vascsilvia@gmail.com>
2022-12-09 02:04:29 -0300

    exercicio de casa

--- exercicios/para-sala/resolucao/classes/exercicio2.js
+++ exercicios/para-sala/resolucao/classes/exercicio2.js
@@ -28,8 +28,8 @@ class InvoiceItem{
     /// o set é o que nesse caso? 
     /// posso fazer assim no return return `Qtd do produto: ${this.qty = qty} `
     setQty(qty){
-        this.qty = qty;
-        return `Qtd do produto: ${this.qty}`;
+        this.quantity = qty;
+        return `Qtd do produto: ${this.quantity}`;
     }
 
     getUnitPrice(){
@@ -47,7 +47,7 @@ class InvoiceItem{
     }
 
     // ????????? é isso? 
-    // ????????? é isso? não entendi 
+    // ????????? é isso? não entendi, ta certo?? 
     toString(){
         return `InvoiceItem[id=${this.id},desc=${this.desc},qty=${
           this.qty
@@ -67,10 +67,10 @@ console.log(newProduct.updateDescription("Descrição nova"))
 
 //Resolução do exercício
 const melao = new InvoiceItem(1, "Melão de Mossoró", 10, 4.50);
-const jaca = new InvoiceItem(2, "Jaca dura", 50, 5.00);
-const caju = new InvoiceItem(3, "Caju vermelho", 80, 1.00); 
+const jaca = new InvoiceItem(2, "Jaca dura", 50, 5);
+const caju = new InvoiceItem(3, "Caju vermelho", 80, 1); 
 
-console.log(melao.getDesc(),'-', melao.getUnitPrice());
-console.log(jaca.getDesc(),'-', jaca.getUnitPrice());
-console.log(caju.getDesc(),'-', caju.getUnitPrice());
+console.log(melao.getDesc(),'-', melao.getQty(),'-', melao.getUnitPrice());
+console.log(jaca.getDesc(),'-', jaca.getQty(),'-', jaca.getUnitPrice());
+console.log(caju.getDesc(),'-', caju.getQty(),'-', caju.getUnitPrice());
 
--- material/heranca/index.js
+++ material/heranca/index.js
@@ -9,6 +9,42 @@ Os benefícios da herança são que os programas podem criar uma classe genéric
 em seguida, criar classes mais específicas a partir da classe genérica, conforme necessário.
 Isso simplifica a programação geral, porque em vez de recriar a estrutura da Person várias vezes, as classes filhas obtêm acesso automaticamente
 às funcionalidades de sua classe mãe.
+
+Herança são classes "filhas" que herdam atributos da classe "mãe"
+*/
+
+/*
+Antes da separação em arquivos
+
+// Classe mãe 
+class Person { 
+    constructor(name, email, cel){ 
+        this.name = name;
+        this.email = email;
+        this.cel = cel;
+    }
+
+    getName(){
+        return `O nome é ${this.name}`;
+    }
+}
+
+// Classe filha
+// A classe Developer vai conter (extends) informações da Person
+class Developer extends Person {
+    // constructor vai pegar os atributos
+    constructor(name, email, cel, skills) { 
+        // super vai fazer referência a classe Person, ele vai mapear/pesquisar as propriedades na Person 
+        // é pelo "super" que vamos pegar as informações do Person
+        super(name, email, cel);
+
+        this.skills = skills;
+    }
+}
+const createPerfilDani = new Developer("Danielle", "dani@email.com", "(11)9999-2212", ["html", "css", "Javascript"]);
+console.log(createPerfilDani);
+console.log(createPerfilDani.getName());
+
 */
 
 
@@ -16,4 +52,5 @@ import { Developer } from './Developer.js';
 const createPerfilDani = new Developer("Danielle", "dani@email.com", "(11)9999-2212", ["html", "css", "Javascript"]);
 
 console.log(createPerfilDani.getName());
+
 
